0141f6f31f3f4346888e48f8c02836edcd952465
harshika212 <harshikasingh212@gmail.com>
2026-04-01 03:30:10 +0530

    Create Array-insert-at-index.java

---
+++ Array-insert-at-index.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public void insertAtIndex(ArrayList<Integer> arr, int index, int val) {
+        // code here
+        arr.add(index, val);
+        }
+    
+    }
