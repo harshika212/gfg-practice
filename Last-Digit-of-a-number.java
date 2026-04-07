705a4d124dc43156061342a407c39b2a4679854e
harshika212 <harshikasingh212@gmail.com>
2026-04-07 23:55:24 +0530

    Create Last-Digit-of-a-number.java

---
+++ Last-Digit-of-a-number.java
@@ -0,0 +1,12 @@
+// Given an integer n. Write a program to find last digit of the number.
+
+class Solution {
+    public static void utility(int n) {
+
+        // just complete below statement
+        int ans = Math.abs(n%10);
+
+            // below statement print the result
+            System.out.println(ans);
+    }
+}
