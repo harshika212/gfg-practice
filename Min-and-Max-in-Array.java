08f471addae7d518abeed34ab46b655c89fd5811
harshika212 <harshikasingh212@gmail.com>
2026-04-07 23:50:35 +0530

    Create Min-and-Max-in-Array.java

---
+++ Min-and-Max-in-Array.java
@@ -0,0 +1,25 @@
+// Given an array arr[]. Your task is to find the minimum and maximum elements in the array.
+
+  class Solution {
+    public ArrayList<Integer> getMinMax(int[] arr) {
+        // code Here
+        ArrayList<Integer> result = new ArrayList<>();
+         int min = arr[0];
+        int max = arr[0];
+        
+        for (int num : arr) {
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+        }
+        
+        result.add(min);
+        result.add(max);
+        
+        return result;
+        
+    }
+}
